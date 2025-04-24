7efb3ff6fecc00b9b4189fcc973031c517e35ce8
Amogh Babu K A <144351704+amogh-babu-k-a@users.noreply.github.com>
2025-04-24 11:28:42 +0530

    Slice Conference 2025
    Co-authored-by: amoghbabuka <amogh_bka@cs.ittr.ac.in>

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@
 /* ==========================================================================
    countdown timer
    ========================================================================== */
- jQuery('#clock').countdown('2024/09/23',function(event){
+ jQuery('#clock').countdown('2025/10/6',function(event){
   var $this=jQuery(this).html(event.strftime(''
   +'<div class="time-entry days"><span>%-D</span> Days</div> '
   +'<div class="time-entry hours"><span>%H</span> Hours</div> '
